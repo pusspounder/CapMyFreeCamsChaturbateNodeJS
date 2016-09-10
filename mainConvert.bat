32f968a4de39a03e3f7ce2a56e2e19fa941925d3
pusspounder <pusspounder>
2016-09-10 18:21:40 +0100

    Fix "mainConvert.bat"

--- mainConvert.bat
+++ mainConvert.bat
@@ -1,15 +1,23 @@
 @echo off
 
+rem used in both CapMyFreeCamsNodeJS and CapChaturbate!
+rem just change "inputfiletype" and "output"
+
 set savedir=%1
 set inputfiletype=*.ts
 set outputfiletype=mkv
 set arg1=%~2
+rem 10mb 10485760
+rem 50mb 52428800
+rem 100mb 104857600
+set minimumsize=52428800
 
 for %%a in (%savedir%\%inputfiletype%) do (
 	call :fix "%%a"
 	)
 
-goto :eof
+rem need exit here to exit command prompt
+exit
 
 rem ----------------------------------------------------------------------------
 
@@ -17,16 +25,13 @@ rem ----------------------------------------------------------------------------
 
 call :setsize %1
 rem delete if less than size in bytes
-rem 10mb 10485760
-rem 50mb 52428800
-rem 100mb 104857600
-if %size% lss 52428800 (
+if %size% lss %minimumsize% (
 del %1
 goto :eof
 )
 
 set output=%1
-set output=%output:~0,-3%%outputfiletype%"
+set output=%output:~0,-3%%outputfiletype%
 
 echo.
 echo ==================================================
@@ -40,13 +45,21 @@ ffmpeg.exe -y -i %1 -c:v copy -c:a copy %output%
 
 if "%arg1%" == "deleteyes" (
 	call :setsize %output%
-	if %size% lss 4096 (del %output%) else (echo "DELETING ORIGINAL FLV" & del %1)
+	if %size% lss %minimumsize% (
+		del %output%
+	) else (
+		echo "DELETING ORIGINAL FLV" & del %1
 	)
+)
 
 if "%arg1%" == "deleteno" (
 	call :setsize %output%
-	if %size% lss 4096 (del %output%) else (echo "NOT DELETING ORIGINAL FLV")
+	if %size% lss %minimumsize% (
+		del %output%
+	) else (
+		echo "NOT DELETING ORIGINAL FLV"
 	)
+)
 
 goto :eof
 
